6d33dd3cfbd492c92de5a7c67599174cc0756b1f
ferdiaa <ferdiindra0@gmail.com>
2021-10-06 01:15:24 +0700

    test technical

---
+++ resources/views/test2.blade.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta name="og:type" content="website" />
+    <meta name="twitter:card" content="photo" />
+    <link rel="stylesheet" type="text/css" href="/dist/css/test2.css" />
+    <link rel="stylesheet" type="text/css" href="/dist/css/styleguide.css" />
+    <link rel="stylesheet" type="text/css" href="/dist/css/globals.css" />
+  </head>
+  <body style="margin: 0; background: #ffffff">
+    <input type="hidden" id="anPageName" name="page" value="test2" />
+    <div class="container-center-horizontal">
+      <form class="test2 screen" name="form6" action="form6" method="post">
+        <div class="group-2416-U0vj8k">
+          <div class="bg-B6DxGO"></div>
+          <div class="transaksi-B6DxGO valign-text-middle">Transaksi</div>
+        </div>
+        <div class="group-427320577-U0vj8k">
+          <div class="rectangle-11-v1DCEX"></div>
+          <div class="group-2433-v1DCEX">
+            <div class="pendapatan-kotor-YSKE1w">PENDAPATAN KOTOR</div>
+            <div class="rp-300000000-YSKE1w">Rp 30.000.00,00</div>
+          </div>
+        </div>
+        <a href="javascript:SubmitForm('form6')">
+          <div class="group-72-U0vj8k">
+            <div class="rectangle-80-p6xbhP"></div>
+            <div class="semua-p6xbhP valign-text-middle">Semua</div>
+          </div></a
+        >
+        <div class="group-71-U0vj8k"><div class="x14-juli-2021-BDAo09 valign-text-middle">14 Juli 2021</div></div>
+        <img class="line-1-U0vj8k" src="/dist/img/line-1@2x.svg" />
+        <div class="frame-90-U0vj8k">
+          <div class="ellipse-52-e2h9cI"></div>
+          <img class="ellipse-3-e2h9cI" src="/dist/img/ellipse-3@2x.png" />
+          <div class="alexander-riuji-e2h9cI poppins-medium-william-15-7px">Alexander Riuji</div>
+          <div class="rp-150000-e2h9cI poppins-medium-harlequin-15-7px">Rp. 150.000</div>
+          <div class="x1245-pm-e2h9cI valign-text-middle poppins-normal-gull-gray-13-1px">LPG 15 Kg</div>
+          <div class="x1245-pm-MfUXxu valign-text-middle poppins-medium-gull-gray-13-1px">Harga</div>
+        </div>
+        <a href="javascript:SubmitForm('form6')">
+          <div class="group-2415-U0vj8k">
+            <div class="group-73-cVicX1">
+              <div class="rectangle-80-22Szok"></div>
+            </div>
+            <div class="group-2413-cVicX1">
+              <div class="group-2414-nKYSJ1">
+                <div class="filter-ZgOF8U valign-text-middle">Filter</div>
+                <div class="options-ZgOF8U"><img class="options-zpkwxw" src="/dist/img/options@2x.svg" /></div>
+              </div>
+            </div></div
+        ></a>
+        <div class="frame-91-U0vj8k">
+          <div class="ellipse-52-9hL8HW"></div>
+          <div class="carlos-naumbabe-9hL8HW poppins-medium-william-15-7px">Carlos Naumbabe</div>
+          <div class="rp-150000-9hL8HW poppins-medium-harlequin-15-7px">Rp. 150.000</div>
+          <div class="x1245-pm-9hL8HW valign-text-middle poppins-normal-gull-gray-13-1px">LPG 15 Kg</div>
+          <div class="x1245-pm-lu9daC valign-text-middle poppins-medium-gull-gray-13-1px">Harga</div>
+          <img class="ellipse-2-9hL8HW" src="/dist/img/ellipse-2@2x.png" />
+        </div>
+        <div class="frame-90-aWtCZu">
+          <div class="ellipse-52-PEzAkU"></div>
+          <div class="sean-widjaya-PEzAkU poppins-medium-william-15-7px">Sean Widjaya</div>
+          <div class="rp-150000-PEzAkU poppins-medium-harlequin-15-7px">Rp. 150.000</div>
+          <div class="x1245-pm-PEzAkU valign-text-middle poppins-normal-gull-gray-13-1px">LPG 15 Kg</div>
+          <div class="x1245-pm-gQ0Pds valign-text-middle poppins-medium-gull-gray-13-1px">Harga</div>
+          <img class="ellipse-3-PEzAkU" src="/dist/img/ellipse-3-1@2x.png" />
+        </div>
+        <div class="frame-92-U0vj8k">
+          <div class="ellipse-52-xkCVet"></div>
+          <div class="angga-kusumah-xkCVet poppins-medium-william-15-7px">Angga Kusumah</div>
+          <div class="rp-150000-xkCVet poppins-medium-harlequin-15-7px">Rp. 150.000</div>
+          <div class="x1245-pm-xkCVet valign-text-middle poppins-normal-gull-gray-13-1px">LPG 15 Kg</div>
+          <div class="x1245-pm-xwWmxx valign-text-middle poppins-medium-gull-gray-13-1px">Harga</div>
+          <img class="ellipse-2-xkCVet" src="/dist/img/ellipse-2-1@2x.png" />
+        </div>
+        <div class="rectangle-11-U0vj8k"></div>
+        <div class="group-427320578-U0vj8k">
+          <div class="group-2412-fT7gMj">
+            <div class="rectangle-11-FACREA"></div>
+            <div class="rectangle-12-FACREA"></div>
+            <div class="group-2411-FACREA">
+              <div class="total-saldo-ffxAfn">TOTAL SALDO</div>
+              <h1 class="title-ffxAfn">Rp 50.000.00,00</h1>
+            </div>
+          </div>
+          <a href="javascript:SubmitForm('form6')">
+            <div class="group-2417-fT7gMj">
+              <div class="frame-70-OoXdXi">
+                <div class="ellipse-52-MwPpA8"></div>
+                <img class="vector-MwPpA8" src="/dist/img/vector@2x.svg" />
+              </div>
+              <div class="group-2418-OoXdXi">
+                <div class="rectangle-80-kZuHVy"></div>
+                <div class="tambah-kZuHVy">Tambah</div>
+              </div>
+            </div></a
+          >
+        </div>
+        <div class="riwayat-tambah-saldo-U0vj8k">Riwayat Tambah Saldo</div>
+        <a href="javascript:SubmitForm('form6')">
+          <div class="group-2422-U0vj8k">
+            <div class="group-18-1v8qg4">
+              <div class="rectangle-31-qr7gDS"></div>
+            </div>
+            <div class="group-2421-1v8qg4">
+              <div class="rp-5000000-22bQZk valign-text-middle poppins-medium-tuna-17-1px">Rp 5.000.000</div>
+              <div class="group-29-22bQZk">
+                <div class="tambah-saldo-ELkLVR poppins-normal-shark-17-1px">Tambah Saldo</div>
+                <div class="x20-juli-2021-ELkLVR poppins-normal-pale-sky-13-1px">20 Juli 2021</div>
+              </div>
+              <div class="group-70-22bQZk">
+                <div class="ellipse-52-La4oVv"></div>
+                <img class="vector-La4oVv" src="/dist/img/vector-1@2x.svg" />
+              </div>
+            </div></div></a
+        ><a href="javascript:SubmitForm('form6')">
+          <div class="group-2423-U0vj8k">
+            <div class="group-18-oUMrFR">
+              <div class="rectangle-31-DdIurt"></div>
+            </div>
+            <div class="group-2421-oUMrFR">
+              <div class="rp-5000000-xlXkaz valign-text-middle poppins-medium-tuna-17-1px">Rp 5.000.000</div>
+              <div class="group-29-xlXkaz">
+                <div class="tambah-saldo-TqqzPb poppins-normal-shark-17-1px">Tambah Saldo</div>
+                <div class="x1-juni-2021-TqqzPb poppins-normal-pale-sky-13-1px">1 Juni 2021</div>
+              </div>
+              <div class="group-70-xlXkaz">
+                <div class="ellipse-52-3lA8Vw"></div>
+                <img class="vector-3lA8Vw" src="/dist/img/vector-2@2x.svg" />
+              </div>
+            </div></div></a
+        ><a href="javascript:SubmitForm('form6')">
+          <div class="group-2424-U0vj8k">
+            <div class="group-18-8dYzpG">
+              <div class="rectangle-31-1tNCi5"></div>
+            </div>
+            <div class="group-2421-8dYzpG">
+              <div class="rp-5000000-x6ScJL valign-text-middle poppins-medium-tuna-17-1px">Rp 5.000.000</div>
+              <div class="group-29-x6ScJL">
+                <div class="tambah-saldo-bSl6ex poppins-normal-shark-17-1px">Tambah Saldo</div>
+                <div class="x15-mei-2021-bSl6ex poppins-normal-pale-sky-13-1px">15 Mei 2021</div>
+              </div>
+              <div class="group-70-x6ScJL">
+                <div class="ellipse-52-969ucM"></div>
+                <img class="vector-969ucM" src="/dist/img/vector-3@2x.svg" />
+              </div>
+            </div></div
+        ></a>
+        <div class="finansial-U0vj8k">Finansial</div>
+      </form>
+    </div>
+    <script>
+      /* Copyright (c) 2016 Tobias Buschor https://goo.gl/gl0mbf | MIT License https://goo.gl/HgajeK */
+
+      if (!HTMLFormElement.prototype.reportValidity) {
+        HTMLFormElement.prototype.reportValidity = function () {
+          if (this.checkValidity()) return true;
+          var btn = document.createElement("button");
+          this.appendChild(btn);
+          btn.click();
+          this.removeChild(btn);
+          return false;
+        };
+      }
+
+      function SubmitForm(form_name) {
+        var form = document.getElementsByName(form_name)[0];
+        if (form.reportValidity()) {
+          form.submit();
+          if (window.submitted) window.submitted();
+        }
+      }
+    </script>
+  </body>
+</html>
